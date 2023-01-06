e8438821c0ac93d485169449bbe3a5246ca17dce
Keita Yokoyama <kayokoya@live.unc.edu>
2023-01-05 19:31:03 -0500

    Partial update to reproduce issue on GitHub Issues

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,34 @@
 module.exports = function (eleventyConfig) {
-    // Return your Object options:
+    const Nunjucks = require('nunjucks');
+
+    // Copy additional files to the output folder
+    eleventyConfig.addPassthroughCopy("src/css/*.css");
+    eleventyConfig.addPassthroughCopy("src/js");
+
+    // Configure Nunjucks template generator
+    let nunjucksEnvironment = new Nunjucks.Environment(
+        new Nunjucks.FileSystemLoader([
+            "src/layouts",
+        ])
+    );
+    eleventyConfig.setLibrary("njk", nunjucksEnvironment);
+
+    // Create filters for some common tasks
+    eleventyConfig.addFilter("getYear", function (date) {
+        const d = new Date(date);
+        return d.getFullYear();
+    });
+
+    // Return Object
     return {
-        dir: {
+        dir: { // all paths except for input/output are relative to the input directory
             input: "src",
             output: "_public",
             data: "data",
-            includes: "layouts"
+            includes: "include",
+            layouts: "layouts",
         },
+        htmlTemplateEngine: "njk",
+        pathPrefix: "/", // for GitHub, change to "/keitaay/"
     }
 };
