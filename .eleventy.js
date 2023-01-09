5343e45d23631fd006fae4d80e5f7da03b29b888
Keita Yokoyama <kayokoya@live.unc.edu>
2023-01-09 13:59:10 -0500

    Glitch corrected, created test page for portfolio

--- .eleventy.js
+++ .eleventy.js
@@ -2,16 +2,8 @@ module.exports = function (eleventyConfig) {
     const Nunjucks = require('nunjucks');
 
     // Copy additional files to the output folder
-    eleventyConfig.addPassthroughCopy("src/css/*.css");
-    eleventyConfig.addPassthroughCopy("src/js");
-
-    // Configure Nunjucks template generator
-    let nunjucksEnvironment = new Nunjucks.Environment(
-        new Nunjucks.FileSystemLoader([
-            "src/layouts",
-        ])
-    );
-    eleventyConfig.setLibrary("njk", nunjucksEnvironment);
+    eleventyConfig.addPassthroughCopy("src/_css/*.css");
+    eleventyConfig.addPassthroughCopy("src/_js");
 
     // Create filters for some common tasks
     eleventyConfig.addFilter("getYear", function (date) {
@@ -25,8 +17,8 @@ module.exports = function (eleventyConfig) {
             input: "src",
             output: "_public",
             data: "data",
-            includes: "include",
-            layouts: "layouts",
+            includes: "_include",
+            layouts: "_layouts",
         },
         htmlTemplateEngine: "njk",
         pathPrefix: "/", // for GitHub, change to "/keitaay/"
